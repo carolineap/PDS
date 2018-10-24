baf96d7064f2241bd4e61c9a0044db0ba119a77c
IsabellaSoares <isabella.lima@dcomp.sor.ufscar.br>
2018-10-24 07:02:25 -0700

    Atualização do BD com dados 2008-2018

---
+++ BD/insert.sql
@@ -0,0 +1,56 @@
+﻿--DADOS DA CAROL
+COPY Boi FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/01-01-2011--13-11-18/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/01-01-2011--13-11-18/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/01-01-2011--13-11-18/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/01-01-2011--13-11-18/Cafe.txt' (DELIMITER(';'));
+
+COPY Boi FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/26-08-17--16-10-18/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/26-08-17--16-10-18/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/26-08-17--16-10-18/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/26-08-17--16-10-18/Cafe.txt' (DELIMITER(';'));
+
+COPY Boi FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/2008 - 2009/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/2008 - 2009/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/2008 - 2009/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/2008 - 2009/Cafe.txt' (DELIMITER(';'));
+
+COPY Boi FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/2010/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/2010/Milho.txt' (DELIMITER(';'));
+--COPY Soja FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/2010/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/crawlerAjustePregaoFuturo/2010/Cafe.txt' (DELIMITER(';'));
+
+--DADOS DA SAYURI
+COPY Boi FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2011/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2011/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2011/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2011/Cafe.txt' (DELIMITER(';'));
+
+COPY Boi FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2012/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2012/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2012/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2012/Cafe.txt' (DELIMITER(';'));
+
+COPY Boi FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2013/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2013/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2013/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2013/Cafe.txt' (DELIMITER(';'));
+
+COPY Boi FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2014/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2014/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2014/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2014/Cafe.txt' (DELIMITER(';'));
+
+COPY Boi FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2015/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2015/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2015/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2015/Cafe.txt' (DELIMITER(';'));
+
+COPY Boi FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2016/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2016/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2016/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2016/Cafe.txt' (DELIMITER(';'));
+
+COPY Boi FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2017/Boi.txt' (DELIMITER(';'));
+COPY Milho FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2017/Milho.txt' (DELIMITER(';'));
+COPY Soja FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2017/Soja.txt' (DELIMITER(';'));
+COPY Cafe FROM '/home/linux/Desktop/PDS/getDatas/DadosProntos/2017/Cafe.txt' (DELIMITER(';'));
